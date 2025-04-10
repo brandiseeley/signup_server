38703eb2c14a47cb5e4209ace4db9b5cf9a192df
Brandi Seeley <brandi.n.seeley@gmail.com>
2025-04-10 18:18:29 +0200

    fix indent

--- app.py
+++ app.py
@@ -81,5 +81,5 @@ def valid_password():
 if __name__ == '__main__':
     if os.environ.get('FLASK_ENV') == 'production':
             app.run(debug=False)
-        else:
-            app.run(debug=True, port=5003)
+    else:
+        app.run(debug=True, port=5003)
